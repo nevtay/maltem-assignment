55e6a7bfa1fc9b1a6e7f3994728edaf4bffe9e9b
nevtay <nevintay@gmail.com>
2020-06-05 00:34:05 +0800

    style: add semicolons

--- tests/index-html.test.js
+++ tests/index-html.test.js
@@ -1,49 +1,49 @@
-const fs = require("fs")
-const path = require("path")
-const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf8")
+const fs = require("fs");
+const path = require("path");
+const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf8");
 
-jest.dontMock("fs")
+jest.dontMock("fs");
 
 describe("index.html", function () {
   beforeEach(() => {
-    document.documentElement.innerHTML = html.toString()
-  })
+    document.documentElement.innerHTML = html.toString();
+  });
 
   afterEach(() => {
-    jest.resetModules()
-  })
+    jest.resetModules();
+  });
   it("body element renders", function () {
-    const bodyTag = document.getElementsByTagName("body")
-    expect(bodyTag).toBeTruthy()
-  })
+    const bodyTag = document.getElementsByTagName("body");
+    expect(bodyTag).toBeTruthy();
+  });
 
   describe("header and container elements", () => {
     it("header and main tags renders", function () {
-      const headerTag = document.getElementsByTagName("header")
-      expect(headerTag).toBeTruthy()
+      const headerTag = document.getElementsByTagName("header");
+      expect(headerTag).toBeTruthy();
 
-      const mainTag = document.getElementsByTagName("main")
-      expect(mainTag).toBeTruthy()
-    })
+      const mainTag = document.getElementsByTagName("main");
+      expect(mainTag).toBeTruthy();
+    });
 
     it("header contains expected text", function () {
-      const headerTag = document.getElementById("header")
-      const expectedText = "Trello Clone"
-      const headerText = headerTag.innerHTML
-      expect(headerText).toEqual(expectedText)
-    })
+      const headerTag = document.getElementById("header");
+      const expectedText = "Trello Clone";
+      const headerText = headerTag.innerHTML;
+      expect(headerText).toEqual(expectedText);
+    });
 
     it("button contains expected text", function () {
-      const addColumnButton = document.getElementById("add-column-button")
-      const expectedText = "Add Column"
-      const addColumnButtonText = addColumnButton.innerHTML
-      expect(addColumnButtonText).toEqual(expectedText)
-    })
+      const addColumnButton = document.getElementById("add-column-button");
+      const expectedText = "Add Column";
+      const addColumnButtonText = addColumnButton.innerHTML;
+      expect(addColumnButtonText).toEqual(expectedText);
+    });
 
     it("search field renders with empty input by default", function () {
-      const searchBox = document.getElementById("search-box")
-      expect(searchBox).toBeTruthy()
-      expect(searchBox.value).toEqual("")
-    })
-  })
-})
+      const searchBox = document.getElementById("search-box");
+      expect(searchBox).toBeTruthy();
+      expect(searchBox.value).toEqual("");
+    });
+  });
+});
