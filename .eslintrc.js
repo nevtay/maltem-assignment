23430bf602cb5734fc8855c4c4187cba05875b08
nevtay <nevintay@gmail.com>
2020-06-05 00:23:34 +0800

    style: reinstall eslint as it wasn't linting on save

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "jest": true
-    },
-    "extends": [
-        "standard"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 11
-    },
-    {
-    "plugins": ["html"]
-    },
-    "rules": {
-        "jest/expect-expect": 0,
-        semi: "error",
-        quotes: ["error", "double"]
-    }
-};
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true
+  },
+  extends: ["standard"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 11
+  },
+  rules: {
+    semi: "error",
+    quotes: ["error", "double"]
+  }
+}
