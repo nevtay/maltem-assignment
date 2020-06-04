734d646b2212163d8ff841af4c83e75886eae512
nevtay <nevintay@gmail.com>
2020-06-04 16:46:33 +0800

    build: add linting rules

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "jest": true
+    },
+    "extends": [
+        "standard"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 11
+    },
+    "rules": {
+        semi: ["error", "always"],
+        quotes: ["error", "double"]
+    }
+};
