9d21d8a72e892cf3144aef25c6efa19ce331a370
nevtay <nevintay@gmail.com>
2020-06-05 23:02:59 +0800

    feat: add code to handle resizing of card description based on focus state and during typing

--- components/card.js
+++ components/card.js
@@ -1,4 +1,14 @@
 const template = document.createElement("template");
+
+const autoResize = (element) => {
+  element.style.height = "5px";
+  element.style.height = (element.scrollHeight) + "px";
+};
+
+const downSize = (element) => {
+  element.style.height = "50px";
+};
+
 template.innerHTML = `
   <style>
 
@@ -13,27 +23,46 @@ template.innerHTML = `
       width: 100%;
     }
 
+    #card-title {
+      min-width: 100;
+      font-size: 14px;
+      font-weight: 700;
+      padding: 3.5px 2.5px 3.5px 2.5px;
+      margin-right: 5px;
+      border-radius: 10px;
+    }
+
+    .card-container-buttons {
+      margin: 10px 0 0 0;
+    }
+
     .card-column-parent {
       font-size: 12px;
     }
 
     .card-description {
-      height: 100px;
+      min-height: 50px;
+      max-height: 200px;
       margin-top: 10px;
       border: none;
       outline: 0px;
       width: 260px;
       transition: all 0.1s;
       resize: none;
+      overflow: hidden;
     }
     
     .card-description:not(:focus) {
-      height: auto;
+      height: 50px;
       border: none;
       outline: 0px;
       background: #d2d2d2;
       appearance: textfield;
       -webkit-appearance: textfield;
+    }
+
+    textarea::-webkit-scrollbar {
+      display: none;
     }
     
   </style>
@@ -41,13 +70,13 @@ template.innerHTML = `
     <div class="card-container">
       <span id="card-title"><slot name="title" /></span>
       <span class="card-column-parent">Column: <em><span id="card-column-parent-text"></span></em></span>
-      <div>
+      <div class="card-container-buttons">
         <button id="deleteBtn">Delete</button>
         <button id="editBtn">Edit</button>
       </div>
     </div>
-    <div>
-      <textarea type="text" class="card-description"}></textarea>
+    <div class="card-container">
+      <textarea type="text" class="card-description" oninput="autoResize(this)" onfocus="autoResize(this)" onblur="downSize(this)"></textarea>
     </div>
   </div>
 `;
