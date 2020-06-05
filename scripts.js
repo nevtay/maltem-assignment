a5fb451d62137ca8f406b73ed8178228be89daf5
nevtay <nevintay@gmail.com>
2020-06-05 21:26:41 +0800

    feat: replace code for generating card components with code for creating column components, and add code to grab titles of all existing columns when page loads/refreshes
    code for creating new columns also includes checks to prevent columns with duplicate or empty titles from being created

--- scripts.js
+++ scripts.js
@@ -1,16 +1,31 @@
 const addColBtn = document.querySelector("#add-column-button");
 const mainDisplayArea = document.querySelector("main");
+const columnTitles = [];
 
-const addNewCard = () => {
-  const newCard = document.createElement("card-contents");
-  const newCardTitle = newCard.shadowRoot.querySelector("#card-title");
-  const cardTitle = prompt("Enter card name");
-  if (!cardTitle.trim()) {
-    alert("Invalid title - card not created");
+const addNewColumn = () => {
+  const newColumn = document.createElement("column-container");
+  const newColumnTitle = newColumn.shadowRoot.querySelector("#column-title");
+  const setColumnTitle = prompt("Name your column");
+  if (columnTitles.includes(setColumnTitle)) {
+    alert("No duplicate titles allowed");
+  } else if (!setColumnTitle || !setColumnTitle.trim()) {
+    alert("Column title cannot be blank");
   } else {
-    newCardTitle.textContent = cardTitle;
-    mainDisplayArea.appendChild(newCard);
+    newColumnTitle.textContent = setColumnTitle;
+    mainDisplayArea.appendChild(newColumn);
   }
 };
 
-addColBtn.addEventListener("click", addNewCard);
+const getAllColumns = () => {
+  const allColumns = document.querySelectorAll("column-container");
+  if (!allColumns) {
+    return null;
+  } else {
+    for (let i = 0; i < allColumns.length; i++) {
+      columnTitles.push(allColumns[i].shadowRoot.querySelector("#column-title").innerText);
+    }
+  }
+};
+
+addColBtn.addEventListener("click", addNewColumn);
+window.addEventListener("load", getAllColumns);
