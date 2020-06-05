298c6dca94f9b5d9047fc1540106c567314315e4
nevtay <nevintay@gmail.com>
2020-06-05 18:47:50 +0800

    feat: add ability to delete column and rename column title

--- components/column.js
+++ components/column.js
@@ -1,9 +1,64 @@
-const template = document.createElement("template");
-template.innerHTML = `
+const columnTemplate = document.createElement("template");
+
+const deleteColumn = (e) => {
+  const columnToRemove = e.target.closest(".column");
+  columnToRemove.remove();
+};
+
+const editColumnTitle = (e) => {
+  const columnTitle = e.target.closest(".column").querySelector("#column-title");
+  if (e.target.id === "editColumnBtn" || e.target.id === "column-title") {
+    columnTitle.setAttribute("contenteditable", "true");
+    columnTitle.focus();
+    columnTitle.style.background = "#f9f9f9";
+    columnTitle.style.padding = "0 10px";
+  } else {
+    columnTitle.setAttribute("contenteditable", "false");
+    columnTitle.blur();
+    columnTitle.style.background = "none";
+    columnTitle.style.padding = "0";
+  };
+};
+
+columnTemplate.innerHTML = `
   <style>
+
+  .column {
+    padding: 30px 15px;
+    height: auto;
+    width: 300px;
+    height: 100px;
+    background: #f9f9f9;
+    margin: 10px 10px;
+  }
+
+  .column-utilities {
+    padding: 10px;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-items: center;
+    background: #e3e3e3;
+  }
+
+  #addCardBtn, #editColumnBtn, #deleteColumnBtn {
+    font-size: 10px;
+  }
+
+
   </style>
   <div class="column">
-  <span class="column-title"><slot name="column-title" /></span>
+    <div class="column-utilities">
+      <div>
+        <span id="column-title"><slot name="column-title" /></span>
+      </div>
+      <div>
+      <button id="addCardBtn">Add Card</button>
+      <button id="editColumnBtn">Edit Column Name</button>
+        <button id="deleteColumnBtn">Delete Column</button>
+      </div>
+    </div>
+    <div class="card-display-area"></div>
   </div>
 `;
 
@@ -12,9 +67,12 @@ class Column extends HTMLElement {
     super();
 
     this.attachShadow({ mode: "open" });
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.appendChild(columnTemplate.content.cloneNode(true));
 
-    this.shadowRoot.querySelector("#column-title").innerText = this.getAttribute("title");
+    this.shadowRoot.querySelector("#column-title").innerText = this.getAttribute("column-title");
+
+    this.shadowRoot.querySelector("#deleteColumnBtn").addEventListener("click", deleteColumn);
+    this.shadowRoot.querySelector(".column").addEventListener("click", editColumnTitle);
   }
 
   connectedCallback () {}
